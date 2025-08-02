7fc04039502b2e100c6e59b54bf07a3c86735837
Christian Penaloza <penalozachrist1108@gmail.com>
2025-08-02 04:06:01 -0500

    nothing

--- forms.py
+++ forms.py
@@ -21,21 +21,20 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me') 
     submit = SubmitField('Login')
 
-# class UpdateUsernameForm(FlaskForm):
-#     username = StringField('New Username', validators=[DataRequired(), Length(min=2, max=20)])
-#     current_password = PasswordField('Current Password', validators=[DataRequired()])
-#     submit_username = SubmitField('Update Username')
+class UpdateUsernameForm(FlaskForm):
+    username = StringField('New Username', validators=[DataRequired(), Length(min=2, max=20)])
+    current_password = PasswordField('Current Password', validators=[DataRequired()])
+    submit_username = SubmitField('Update Username')
 
-# class UpdateEmailForm(FlaskForm):
-#     email = StringField('New Email', validators=[DataRequired(), Email()])
-#     current_password = PasswordField('Current Password', validators=[DataRequired()])
-#     submit_email = SubmitField('Update Email')
+class UpdateEmailForm(FlaskForm):
+    email = StringField('New Email', validators=[DataRequired(), Email()])
+    current_password = PasswordField('Current Password', validators=[DataRequired()])
+    submit_email = SubmitField('Update Email')
 
-# class ChangePasswordForm(FlaskForm):
-#     current_password = PasswordField('Current Password', validators=[DataRequired()])
-#     new_password = PasswordField('New Password', validators=[DataRequired(), Length(min=6)])
-#     confirm_new_password = PasswordField('Confirm New Password', validators=[
-#         DataRequired(), EqualTo('new_password', message='Passwords must match')
-#     ])
-#     submit_password = SubmitField('Change Password')
-
+class ChangePasswordForm(FlaskForm):
+    current_password = PasswordField('Current Password', validators=[DataRequired()])
+    new_password = PasswordField('New Password', validators=[DataRequired(), Length(min=6)])
+    confirm_new_password = PasswordField('Confirm New Password', validators=[
+        DataRequired(), EqualTo('new_password', message='Passwords must match')
+    ])
+    submit_password = SubmitField('Change Password')
